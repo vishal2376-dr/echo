3c0565c8e9d950ada033b74ffc22555fab76fd32
Vishal Singh <vishal.singh@dailyrounds.org>
2026-01-07 11:00:04 +0530

    bump version 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.vishal2376.echo"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
